183496e69b4efebc7ca013bb6df3f865ee7a28da
rahimn20 <80053655+rahimn20@users.noreply.github.com>
2022-08-20 14:55:21 +0500

    test-commit

---
+++ App.js
@@ -0,0 +1,32 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const students = require("./students")
+// const port = 4000;
+// const app = express();
+const database = require("./db")
+
+// mongoose.connect(MONGO_URI)
+
+// app.get("/api", (req, res) => {
+//   res.json({ "rows": [
+//     {
+//         "id": 1,
+//       "name": "Rahim",
+//       "sex": "male",
+//       "place": "Lahore",
+      
+//     },
+//     {
+//         "id": 2,
+//       "name": "Hello There",
+//       "sex": "male",
+//       "place": "Lahore",
+//     }] });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Port listening to: ${port}`);
+// });
+
+students;
+database;
